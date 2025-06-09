d5e94eb6f68138e08a0d4a984fd660f8b3fe3c95
mili <kamil.maksudov2019@gmail.com>
2025-06-09 16:42:34 +0200

    feat: subapase init

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
+  manifest: {
+    permissions: ["storage", "activeTab"],
+    host_permissions: [
+      "*://*.google.com/*",
+      "*://*.bing.com/*",
+      "*://*.duckduckgo.com/*",
+      "*://*.yahoo.com/*",
+    ],
+  },
 });
